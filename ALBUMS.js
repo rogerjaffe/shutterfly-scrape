9e79cc2d099467661e8f573ff868ff71d426f774
Roger Jaffe <rogerjaffe@gmail.com>
2023-02-19 21:56:53 -0800

    Update to better system

--- ALBUMS.js
+++ ALBUMS.js
@@ -1,18 +1,28 @@
-const ALBUMS = [
-  {
-    url: "https://url.of.album.1", // URL of album
-    album: "Album 1 name", // Name of album
-    start: 2, // Starting Image Number
-    end: 50, // Ending Image Number
-    forward: false, // If true, images will be saved in ascending order, otherwise descending order
-  },
-  {
-    url: "https://url.of.album.2", // URL of album
-    album: "Album 2 name", // Name of album
-    start: 83, // Starting Image Number
-    end: 99, // Ending Image Number
-    forward: true, // If true, images will be saved in ascending order, otherwise descending order
-  },
-];
+/**
+ * Album definitions go here
+ * Find the URL by going to the shared Shutterfly album, click on the Pictures or
+ * Pictures & Videos link, then click All to show all the picture thumbnails.
+ * Copy the URL from the browser address bar and paste it here as the value of url
+ * @input {string} url - URL of Shutterfly album index page
+ * @input {string} name - Name of album
+ * @input {string} folder - Folder name to save images to
+ *
+ * @type {{albums: [{folder: string, name: string, url: string},{folder: string, name: string, url: string}], name: string}}
+ */
+const ALBUMS = {
+  name: "Bert and Ernie's Photo Albums",
+  albums: [
+    {
+      url: "URL OF SHUTTERFLY ALBUM INDEX SHOWING ALL ALBUMS #1",
+      name: "Album 1",
+      folder: "Album1",
+    },
+    {
+      url: "URL OF SHUTTERFLY ALBUM INDEX SHOWING ALL ALBUMS #2",
+      name: "Album 2",
+      folder: "Album2",
+    },
+  ],
+};
 
 export default ALBUMS;
--- index.js
+++ index.js
@@ -1,26 +1,72 @@
 import Promise from "bluebird";
 import * as fs from "fs";
-import { sprintf } from "sprintf-js";
-import download from "./download.js";
-import ALBUMS from "./ALBUMS-AJ2.js";
+import ALBUMS from "./ALBUMS.js";
 import wget from "node-wget";
+import { format, fromUnixTime } from "date-fns";
+import fetch, { Headers } from "node-fetch";
+import JSON5 from "json5";
 
 const SF_URL_PREFIX = "https://uniim-share.shutterfly.com/v2/procgtaserv/";
 const OUT_DIR = "./albums/";
 
-const processPix = async ({ jsonFile, album, folder }) => {
-  console.log(jsonFile, album, folder);
-  const dataJSON = fs.readFileSync(jsonFile, { encoding: "utf8" });
-  const data = JSON.parse(dataJSON);
+const createIndexHtml = (imageFName, text, idx, date, title) => {
+  return `
+    <div class="photo">
+      <a href="${imageFName}" target="_blank">
+        <img src="${imageFName}" />
+      </a>
+    <p class="caption"><span>&#35;${idx + 1}:</span> ${text}</p>
+    <p class="title-date"><span>${title} | ${format(
+    fromUnixTime(date),
+    "MMM d yyyy h:mm a O"
+  )}</span></p>
+    </div>
+  `;
+};
+
+const getJsonData = async (url) => {
+  return new Promise((resolve, reject) => {
+    const tokens = url.split("/");
+    const nodeId = tokens[tokens.length - 1];
+    const albumId = tokens[2].split(".")[0];
+    const body = new URLSearchParams();
+    body.append("startIndex", "0");
+    body.append("size", "-1");
+    body.append("pageSize", "-1");
+    body.append("page", `${albumId}/pictures`);
+    body.append("nodeId", `${nodeId}`);
+    body.append("layout", "ManagementAlbumPictures");
+
+    const fetchUrl = `https://cmd.shutterfly.com/commands/pictures/getitems?site=${albumId}`;
+    fetch(fetchUrl, {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body,
+    })
+      .then((res) => res.text())
+      .then((sortOfJson) => {
+        const json = JSON5.parse(sortOfJson);
+        resolve(json);
+      })
+      .catch((err) => {
+        reject(err);
+      });
+  });
+};
+
+const processPix = async (data, name, folder) => {
   const outDir = `${OUT_DIR}${folder}`;
   fs.mkdirSync(outDir, { recursive: true });
   const items = data.result.section.items;
-  const itemData = await Promise.all(
-    items.map(async (item, idx) => {
-      const { captureDate, text, title, shutterflyId: uri } = item;
+  const itemData = await Promise.map(
+    items,
+    async (item, idx) => {
+      const { captureDate, text = "", title = "", shutterflyId: uri } = item;
+      console.log(`${folder} => Retrieving item ${idx + 1}`);
       const url = SF_URL_PREFIX + uri;
       const [fname, ext] = title.split(".");
-      const fName = `${outDir}/${(idx + 1 + "").padStart(4, "0")}.`;
+      const imageFSpec = `p${(idx + 1 + "").padStart(4, "0")}.${ext}`;
+      const fName = `${outDir}/p${(idx + 1 + "").padStart(4, "0")}.`;
       const imageFName = fName + ext;
       const textFName = fName + "txt";
       await new Promise((resolve, reject) => {
@@ -34,12 +80,83 @@ const processPix = async ({ jsonFile, album, folder }) => {
         });
       });
       fs.writeFileSync(textFName, text);
-      return { text, title, captureDate };
-    })
+      const html = createIndexHtml(imageFSpec, text, idx, captureDate, title);
+      return { text, title, captureDate, html };
+    },
+    { concurrency: 5 }
   );
-  console.log(itemData);
+  const htmlItems = itemData.map((item) => item.html).join("");
+  const html = `
+    <html><head><style>
+    body {background: #eee; font-family: Arial;}
+    .photo {width: 80%; margin-left: auto; margin-right: auto; padding-bottom: 20px; margin-bottom: 30px; border-bottom: 2px solid rgba(0,0,0,0.15);}
+    img {max-height: 80vh; max-width: 100vw; display:block;margin: 0 auto 20px;}
+    .album-title {font-size:3rem; text-align: center; margin: 25px auto; width: 80%; background-color: #ccc; border-radius: 10px; padding: 10px;}
+    .caption {font-family: sans-serif; font-size: 1.5em; color: rgba(0,0,0,0.8); text-align: center;}
+    .title-date {font-family: sans-serif; font-size: 0.9em; font-style: italic; color: rgba(0,0,0,0.5); text-align: center;}
+    </style>
+    <link rel="shortcut icon" href="https://cdn.staticsfly.com/shr/images/favicon/favicon.ico"> 
+    </head>
+    <body>
+    <div class="album-title">${name}</div>
+    ${htmlItems}
+    </body></html>
+  `;
+  fs.writeFileSync(`${outDir}/index.html`, html);
+  return { itemData, count: data.result.section.count };
 };
 
-ALBUMS.albums.forEach((album) => {
-  processPix(album);
+const albumObj = await Promise.map(
+  ALBUMS.albums,
+  async (album) => {
+    console.log(`${album.name} => Retrieve JSON`);
+    const jsonData = await getJsonData(album.url);
+    console.log(`${album.name} => Processing`);
+    const { itemData, count } = await processPix(
+      jsonData,
+      album.name,
+      album.folder
+    );
+    console.log(`${album.name} => Completed`);
+    return { album, itemData, count };
+  },
+  { concurrency: 2 }
+);
+
+// const albumObj = await Promise.map(promises, { concurrency: 2 });
+
+let innerHtml = albumObj.map((album) => {
+  console.log(`${album.album.name} => Create album index items`);
+  const html = `
+    <div class="album-index">
+      <p class="album-name"><a href="./${album.album.folder}/index.html">${album.album.name}</a></p>
+      <p class="album-count">${album.count} pictures</p></p>
+    </div>
+  `;
+  return html;
 });
+
+console.log(`Create album index`);
+innerHtml = innerHtml.join("");
+const indexHtml = `
+  <html><head><style>
+    body {background: #eee; font-family: arial;}
+    .photo {width: 80%; margin-left: auto; margin-right: auto; padding-bottom: 20px; margin-bottom: 30px; border-bottom: 2px solid rgba(0,0,0,0.15);}
+    img {max-height: 80vh; max-width: 100vw; display:block;margin: 0 auto 20px;}
+    p {font-size: 1.5em; color: rgba(0,0,0,0.8); text-align: center;}
+    .album-index {margin: 20px auto; border-radius: 10px; background-color: #ccc; width: 50vw; border: 2px solid #888;}
+    .album-count {font-size: 1.25rem; font-style: italic;}
+    .album-name {font-size: 2rem;}
+    .title {font-size: 2.5rem; text-align: center; margin: 25px;}
+    </style>
+    <link rel="shortcut icon" href="https://cdn.staticsfly.com/shr/images/favicon/favicon.ico"> 
+    </head>
+    <body>
+      <div class="title">${ALBUMS.name}</div>
+      <div class="contents">${innerHtml}</div>
+    </body>
+  </html>
+`;
+fs.writeFileSync(`${OUT_DIR}index.html`, indexHtml);
+
+console.log(`Process complete!`);
